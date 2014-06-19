e55d0d6846cd2fffb2dcb55719f65c57e58c409d
virtugo <virtugo@ya.ru>
2014-06-19 16:14:05 +0400

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #define SIZEY 20
 
 int *FillArray(int, int, int**); // заполняет массив
+int DynamicToStatic(int stWorld[SIZEX][SIZEY], int, int, int**);
+int PrintArray(int stWorld[SIZEX][SIZEY]);
 int *ClearMemory(int, int, int**); // очищаем память
 
 int main()
@@ -13,42 +15,26 @@ int main()
     // динамический массив
     int **dynWorld;
     int dynXM = 100; //30
-	int dynYM = 20; //20
-	// статический массив
-	int stWorld[SIZEX][SIZEY];
+    int dynYM = 20; //20
 
-	int sdvig=0;
+    // статический массив
+    int stWorld[SIZEX][SIZEY];
+    // задаем сдвиг по X (можно поиграть с этим числом для наглядности)
+    int sdvig=2;
+    int i;
 
-	int i,j;
-	// Выделяем память под адреса столбцов
-	// важно сначала выделить под иксы, потом под игреки,
-	// чтобы был вид [i][j], а не [j][i]
-	dynWorld=(int **)malloc(sizeof(int *)*dynXM);
-	// Выделяем память для iго столбца
-	// пробегаемся по всем столбцам с нулевого до последнего
-	// опираясь на кол-во элементов (y-ков)
-	for(i=0;i<dynXM;i++)dynWorld[i]=(int *)malloc(sizeof(int)*dynYM);
+    // инициализируем динамический массив
+    dynWorld=(int **)malloc(sizeof(int *)*dynXM);
+    for(i=0;i<dynXM;i++)dynWorld[i]=(int *)malloc(sizeof(int)*dynYM);
 
-	FillArray(dynXM, dynYM, dynWorld);
+    // заполняем массив
+    FillArray(dynXM, dynYM, dynWorld);
 
-	// передаем часть динамического массива в статический
-	// задаем сдвиг по X (можно поиграть с этим числом для наглядности)
-	sdvig=4;
-
-	for(j=0;j<dynYM;j++){
-        for(i=sdvig;i<SIZEX+sdvig;i++){
-            stWorld[i-sdvig][j]=dynWorld[i][j];
-        }
-	}
+    // передаем часть динамического массива в статический
+    DynamicToStatic(stWorld, sdvig, dynYM, dynWorld);
 
     // выводим на экран статический массив
-
-	for(j=0;j<SIZEY;j++){
-        for(i=0;i<SIZEX;i++){
-            printf("%d ", stWorld[i][j]);
-        }
-        printf("\n");
-	}
+    PrintArray(stWorld);
 
     // освобождаем память
     ClearMemory(dynXM, dynYM, dynWorld);
@@ -58,23 +44,45 @@ int main()
 
 int *FillArray(int XM, int YM, int **dynWorld)
 {
-	int i,j;
+    int i,j;
 
-	for(j=0;j<YM;j++){
+    for(j=0;j<YM;j++){
         for(i=0;i<XM;i++){
             dynWorld[i][j]=0;
         }
-	}
-	// элемент X4 Y3 будет равен 5
-	dynWorld[4][3]=5;
+    }
+    // элемент X4 Y3 будет равен 5
+    dynWorld[4][3]=5;
 
-	return *dynWorld;
+    return *dynWorld;
+}
+
+int DynamicToStatic(int stWorld[SIZEX][SIZEY], int sdvig, int YM, int **dynWorld)
+{
+    int i,j;
+
+    for(j=0;j<YM;j++){
+            for(i=sdvig;i<SIZEX+sdvig;i++){
+                stWorld[i-sdvig][j]=dynWorld[i][j];
+            }
+    }
+}
+
+int PrintArray(int stWorld[SIZEX][SIZEY])
+{
+    int i,j;
+    for(j=0;j<SIZEY;j++){
+            for(i=0;i<SIZEX;i++){
+                printf("%d ", stWorld[i][j]);
+            }
+            printf("\n");
+    }
 }
 
 int *ClearMemory(int XM, int YM, int **dynWorld)
 {
     int i;
-    
+
     for(i=0;i<XM;i++)free(dynWorld[i]);
     free((void *)dynWorld);
 
