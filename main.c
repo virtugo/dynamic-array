b6d82bf2e6c25b66b62963ee54de28126710ab84
virtugo <virtugo@ya.ru>
2014-06-19 16:46:40 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZEX 30// размеры массива для вывода на экран
+#define SIZEX 25// размеры массива для вывода на экран
 #define SIZEY 20
 
 int *FillArray(int, int, int**); // заполняет массив
@@ -9,46 +9,40 @@ int DynamicToStatic(int stWorld[SIZEX][SIZEY], int, int, int**);
 int PrintArray(int stWorld[SIZEX][SIZEY]);
 int *ClearMemory(int, int, int**); // очищаем память
 
+// ============================= MAIN ================================
 int main()
 {
-
     // динамический массив
     int **dynWorld;
     int dynXM = 100; //30
     int dynYM = 20; //20
-
     // статический массив
     int stWorld[SIZEX][SIZEY];
     // задаем сдвиг по X (можно поиграть с этим числом для наглядности)
-    int sdvig=2;
+    int sdvig=70;
     int i;
-
     // инициализируем динамический массив
     dynWorld=(int **)malloc(sizeof(int *)*dynXM);
     for(i=0;i<dynXM;i++)dynWorld[i]=(int *)malloc(sizeof(int)*dynYM);
-
     // заполняем массив
     FillArray(dynXM, dynYM, dynWorld);
-
     // передаем часть динамического массива в статический
     DynamicToStatic(stWorld, sdvig, dynYM, dynWorld);
-
     // выводим на экран статический массив
     PrintArray(stWorld);
-
     // освобождаем память
     ClearMemory(dynXM, dynYM, dynWorld);
-
     return 0;
 }
 
+// ============================= MAIN ================================
 int *FillArray(int XM, int YM, int **dynWorld)
 {
     int i,j;
 
     for(j=0;j<YM;j++){
         for(i=0;i<XM;i++){
-            dynWorld[i][j]=0;
+            dynWorld[i][j]=i;
         }
     }
     // элемент X4 Y3 будет равен 5
@@ -62,9 +56,9 @@ int DynamicToStatic(int stWorld[SIZEX][SIZEY], int sdvig, int YM, int **dynWorld
     int i,j;
 
     for(j=0;j<YM;j++){
-            for(i=sdvig;i<SIZEX+sdvig;i++){
-                stWorld[i-sdvig][j]=dynWorld[i][j];
-            }
+        for(i=sdvig;i<SIZEX+sdvig;i++){
+            stWorld[i-sdvig][j]=dynWorld[i][j];
+        }
     }
 }
 
