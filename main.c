e3e32ce7cb9779e0901609ab29f84a48b86d927c
virtugo <virtugo@ya.ru>
2014-06-20 10:10:22 +0400

    Update main.c

--- main.c
+++ main.c
@@ -5,81 +5,93 @@
 #define SIZEX 25
 #define SIZEY 20
 
-int *FillArray(int, int, int**); // заполняет массив
+int FillArray(int, int, int**); // заполняем массив
 int DynamicToStatic(int stWorld[][SIZEY], int, int, int**);
-int PrintArray(int stWorld[][SIZEY]);
-int *ClearMemory(int, int, int**); // очищаем память
+int PrintArray(int stWorld[][SIZEY]); 
+int ClearMemory(int, int, int**); // очищаем память
 
 // ============================= MAIN ================================
 int main()
 {
-    // динамический массив
-    int **dynWorld;
-    int dynXM = 100;
-    int dynYM = 20;
-    // статический массив
-    int stWorld[SIZEX][SIZEY];
-    // задаем сдвиг по X (можно поиграть с этим числом для наглядности)
-    int sdvig=70;
-    int i;
-    // инициализируем динамический массив
-    dynWorld=(int **)malloc(sizeof(int *)*dynXM);
-    for(i=0;i<dynXM;i++)dynWorld[i]=(int *)malloc(sizeof(int)*dynYM);
-    // заполняем массив
-    FillArray(dynXM, dynYM, dynWorld);
-    // передаем часть динамического массива в статический
-    DynamicToStatic(stWorld, sdvig, dynYM, dynWorld);
-    // выводим на экран статический массив
-    PrintArray(stWorld);
-    // освобождаем память
-    ClearMemory(dynXM, dynYM, dynWorld);
-    return 0;
+	// динамический массив
+	int **dynWorld;
+	int dynXM = 100;
+	int dynYM = 20;
+	// статический массив
+	int stWorld[SIZEX][SIZEY];
+	// задаем сдвиг по X (можно поиграть с этим числом для наглядности)
+	int sdvig = 40;
+	int i;
+	// инициализируем динамический массив
+	dynWorld = (int **)malloc(sizeof(int *)*dynXM);
+	for (i = 0; i<dynXM; i++)dynWorld[i] = (int *)malloc(sizeof(int)*dynYM);
+	// заполняем массив
+	FillArray(dynXM, dynYM, dynWorld);
+	// передаем часть динамического массива в статический
+	DynamicToStatic(stWorld, sdvig, dynYM, dynWorld);
+	// выводим на экран статический массив
+	PrintArray(stWorld);
+	// освобождаем память
+	ClearMemory(dynXM, dynYM, dynWorld);
+
+	system("pause");
+	return 0;
 }
 
 // ============================= OTHER ================================
-int *FillArray(int XM, int YM, int **dynWorld)
+
+// заполняем массив
+int FillArray(int XM, int YM, int **dynWorld)
 {
-    int i,j;
+	int i, j;
 
-    for(j=0;j<YM;j++){
-        for(i=0;i<XM;i++){
-            dynWorld[i][j]=i;
-        }
-    }
-    // элемент X4 Y3 будет равен 5
-    dynWorld[4][3]=5;
+	for (j = 0; j<YM; j++){
+		for (i = 0; i<XM; i++){
+			dynWorld[i][j] = i;
+		}
+	}
+	// элемент X4 Y3 будет равен 5
+	dynWorld[4][3] = 5;
 
-    return *dynWorld;
+	//return *dynWorld;
+	return 0;
 }
 
+// преобразуем динамический массив в статический
 int DynamicToStatic(int stWorld[][SIZEY], int sdvig, int YM, int **dynWorld)
 {
-    int i,j;
+	int i, j;
 
-    for(j=0;j<YM;j++){
-        for(i=sdvig;i<SIZEX+sdvig;i++){
-            stWorld[i-sdvig][j]=dynWorld[i][j];
-        }
-    }
+	for (j = 0; j<YM; j++){
+		for (i = sdvig; i<SIZEX + sdvig; i++){
+			stWorld[i - sdvig][j] = dynWorld[i][j];
+		}
+	}
+
+	return 0;
 }
 
+// печатаем массив
 int PrintArray(int stWorld[][SIZEY])
 {
-    int i,j;
-    for(j=0;j<SIZEY;j++){
-            for(i=0;i<SIZEX;i++){
-                printf("%d ", stWorld[i][j]);
-            }
-            printf("\n");
-    }
+	int i, j;
+	for (j = 0; j<SIZEY; j++){
+		for (i = 0; i<SIZEX; i++){
+			printf("%d ", stWorld[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
 }
 
-int *ClearMemory(int XM, int YM, int **dynWorld)
+// чистим память
+int ClearMemory(int XM, int YM, int **dynWorld)
 {
-    int i;
+	int i;
 
-    for(i=0;i<XM;i++)free(dynWorld[i]);
-    free((void *)dynWorld);
+	for (i = 0; i<XM; i++)free(dynWorld[i]);
+	free((void *)dynWorld);
 
-    return *dynWorld;
+	return 0;
 }
